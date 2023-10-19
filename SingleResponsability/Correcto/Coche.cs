5f8290496104fb50315a9921c87dc3ce49bc13cd
jordiascension <jordiascension@enterpriseconsulting.es>
2023-10-19 12:38:36 +0200

    Added Single Responsability Principle implementatation Closes #1

---
+++ SingleResponsability/Correcto/Coche.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.SingleResponsability.Correcto
+{
+    public class Coche
+    {
+        String marca;
+
+        Coche(String marca) { this.marca = marca; }
+
+        String getMarcaCoche() { return marca; }
+    }
+}
---
+++ SingleResponsability/Incorrecto/Coche.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.SingleResponsability.Incorrecto
+{
+    public class Coche
+    {
+        String marca;
+
+        Coche(String marca) { this.marca = marca; }
+
+        String getMarcaCoche() { return marca; }
+
+        void guardarCocheDB(Coche coche)
+        {
+
+        }
+    }
+}
